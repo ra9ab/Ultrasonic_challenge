0910e7c1248f6e5e6de63cdeb65588b6ae36365a
MinaRaoufAziz <55676799+MinaRaoufAziz@users.noreply.github.com>
2019-10-01 17:58:12 +0200

    Update application.h

--- application.h
+++ application.h
@@ -1,6 +1,11 @@
 #ifndef APPLICATION_H
 #define APPLICATION_H
 
+#define LED0 0
+#define LED1 1
+#define LED2 2
+#define LED3 3
+
 sint8 detect_object(void);
 uint32 count_object(sint8);
 void led_on(uint8);
