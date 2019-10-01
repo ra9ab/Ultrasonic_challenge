3f4a3746fff0e3a9f8e1fb798942befbebb1592b
MinaRaoufAziz <55676799+MinaRaoufAziz@users.noreply.github.com>
2019-10-01 18:14:27 +0200

    Update application.h

--- application.h
+++ application.h
@@ -5,11 +5,13 @@
 #define LED1 1
 #define LED2 2
 #define LED3 3
+#define MAX_DELAY_FACTOR 20
 
 sint8 detect_object(void);
 uint32 count_object(sint8);
 void led_on(uint8);
 void led_off(uint8);
+void distance_alarm(f32);
 
 
 #endif
