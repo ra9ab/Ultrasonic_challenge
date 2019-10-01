49f96c23cb839c14a3b326438f469ea9c3eb4c05
MinaRaoufAziz <55676799+MinaRaoufAziz@users.noreply.github.com>
2019-10-01 21:02:03 +0200

    Update LCD.h

--- LCD.h
+++ LCD.h
@@ -3,7 +3,6 @@
 #define LCD_H_
 
 
-#define F_CPU 8000000ul
 #include <avr/io.h>
 #include <util/delay.h>
 #define DATA_PORT PORTA
@@ -16,17 +15,17 @@
 #define RW 2
 
 void Enable();
-void CMD_WRT_8bit(uint8 cmd);
-void CMD_WRT_4bit(uint8 cmd);
-void DATA_WRT_8bit(uint8 data);
-void DATA_WRT_4bit(uint8 data);
+void CMD_WRT_8bit(uint8_t cmd);
+void CMD_WRT_4bit(uint8_t cmd);
+void DATA_WRT_8bit(uint8_t data);
+void DATA_WRT_4bit(uint8_t data);
 void LCD_INIT_4bit();
 void LCD_CLR();
 void LCD_Cursor_Home();
-void LCD_CUSRSOR_POS(uint8 row , uint8 col);
-void LCD_CHAR_DISP(uint8 row , uint8 col , uint8 data);
-void LCD_String_DISP(uint8 row , uint8 col ,uint8 *s);
-void LCD_NUM_DISP(uint8 row , uint8 col ,uint32 num);
+void LCD_CUSRSOR_POS(uint8_t row , uint8_t col);
+void LCD_CHAR_DISP(uint8_t row , uint8_t col , uint8_t data);
+void LCD_String_DISP(uint8_t row , uint8_t col ,char *s);
+void LCD_NUM_DISP(uint8_t row , uint8_t col ,int num);
 
 
 #endif
