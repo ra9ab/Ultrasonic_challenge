6149233e53ce41158bba8f25791dd50618e2928c
MinaRaoufAziz <55676799+MinaRaoufAziz@users.noreply.github.com>
2019-10-01 21:01:39 +0200

    Create DIO_cnfg.c

---
+++ DIO_cnfg.c
@@ -0,0 +1,24 @@
+/*
+ * DIO_cnfg.c
+ *
+ * Created:  23 , 9 , 2019
+ *  Author: Peter
+ */
+#include "DIO.h"
+extern DIO_cnfg_pin DIO_cnfg_arr[NUM_OF_PINS];
+#define NA 0x55
+
+DIO_cnfg_pin DIO_cnfg_arr [NUM_OF_PINS] =
+{
+	{PORT_C,PIN0,OUTPUT,LOW,NA},
+	{PORT_C,PIN1,OUTPUT,LOW,NA},
+	{PORT_D,PIN0,OUTPUT,HIGH,NA},
+	{PORT_D,PIN1,OUTPUT,HIGH,NA},
+	{PORT_D,PIN2,OUTPUT,HIGH,NA},
+	{PORT_D,PIN3,OUTPUT,HIGH,NA},
+	{PORT_D,PIN4,OUTPUT,HIGH,NA},
+	{PORT_D,PIN5,OUTPUT,HIGH,NA},
+	{PORT_D,PIN6,OUTPUT,HIGH,NA},
+	{PORT_B,PIN1,OUTPUT,HIGH,NA},
+
+};
