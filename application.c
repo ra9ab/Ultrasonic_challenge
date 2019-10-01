46342aee7a963c2be57c8300b45cfe62b95b51d6
MinaRaoufAziz <55676799+MinaRaoufAziz@users.noreply.github.com>
2019-10-01 17:28:51 +0200

    Create application.c
    initial application functions commit

---
+++ application.c
@@ -0,0 +1,97 @@
+#include "application.h"
+
+
+sint8 detect_object()
+{
+	uint32 result = Time_Calc();
+	if(result == MAX_TON)
+	{
+		return object_detected = 0; // object not detected
+	}
+	else if (result < MAX_TON)
+	{
+		return object_detected = 1; // object detected. As the distance is greater than 0 && TON is less than 40 milliseconds
+	}
+	else 
+	{
+		return object_detected = -1;
+	}
+}
+
+uint32 count_object(object_detected)
+{
+	static sint8 is_object_detected = 10;
+	static uint32 detect_counter = 0;
+	if (is_object_detected == 10)
+	{
+		is_object_detected = object_detected;
+	}
+	if (is_object_detected == object_detected)
+	{
+		// Do nothing
+	}
+	else 
+	{
+		detect_counter++;
+		is_object_detected = object_detected;
+	}
+	return detect_counter;
+}
+
+void led_on(uint8 led_no)
+{
+	switch (led_no)
+	{
+		case LED0:
+		DIO_set_pin_direction(PORT_B, PIN4,OUTPUT);
+		DIO_write_pin_value(PORT_B, PIN4,HIGH);
+		break;
+		
+		case LED1:
+		DIO_set_pin_direction(PORT_B, PIN5,OUTPUT);
+		DIO_write_pin_value(PORT_B, PIN5,HIGH);
+		break;
+		
+		case LED2:
+		DIO_set_pin_direction(PORT_B, PIN6,OUTPUT);
+		DIO_write_pin_value(PORT_B, PIN6,HIGH);
+		break;
+		
+		case LED3:
+		DIO_set_pin_direction(PORT_B, PIN7,OUTPUT);
+		DIO_write_pin_value(PORT_B, PIN7,HIGH);
+		break;
+		
+		default:
+		break;
+	}
+}
+
+void led_off(uint8 led_no)
+{
+	switch (led_no)
+	{
+		case LED0:
+		DIO_set_pin_direction(PORT_B, PIN4,OUTPUT);
+		DIO_write_pin_value(PORT_B, PIN4,LOW);
+		break;
+		
+		case LED1:
+		DIO_set_pin_direction(PORT_B, PIN5,OUTPUT);
+		DIO_write_pin_value(PORT_B, PIN5,LOW);
+		break;
+		
+		case LED2:
+		DIO_set_pin_direction(PORT_B, PIN6,OUTPUT);
+		DIO_write_pin_value(PORT_B, PIN6,LOW);
+		break;
+		
+		case LED3:
+		DIO_set_pin_direction(PORT_B, PIN7,OUTPUT);
+		DIO_write_pin_value(PORT_B, PIN7,LOW);
+		break;
+		
+		default:
+		break;
+	}
+}
