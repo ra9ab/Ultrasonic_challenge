f497e54797f8c8ce1007386a375b906884e00ce2
MinaRaoufAziz <55676799+MinaRaoufAziz@users.noreply.github.com>
2019-10-01 21:01:14 +0200

    Update application.h

--- application.h
+++ application.h
@@ -1,6 +1,8 @@
 #ifndef APPLICATION_H
 #define APPLICATION_H
 
+#include "stdtypes.h"
+
 #define LED0 0
 #define LED1 1
 #define LED2 2
@@ -15,7 +17,7 @@ sint8 detect_object(void);
 uint32 count_object(sint8);
 void led_on(uint8);
 void led_off(uint8);
-void distance_alarm(f32);
+void distance_alarm(float32);
 
 
 #endif
