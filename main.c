e9d3206270ee37fa65b1606413ba3c3da5d77a5e
MinaRaoufAziz <55676799+MinaRaoufAziz@users.noreply.github.com>
2019-10-01 18:14:37 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	LCD_INIT_4bit();
+	//trigger function from ehab
+	while(1)
+	{
+		sint8 detect_object_result = detect_object();
+		uint32 counter_dectected_object = count_object(detect_object_result);
+		if (detect_object_result == 1)
+		{
+			led_on(LED0);
+		}
+		else
+		{
+			led_off(LED0);
+		}
+		
+	}
+}
