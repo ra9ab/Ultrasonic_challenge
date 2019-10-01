d87c099fe980a7ce8f01f5c8fee58e049daaae7b
MinaRaoufAziz <55676799+MinaRaoufAziz@users.noreply.github.com>
2019-10-01 21:02:09 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
+#include "application.h"
+#include "DIO.h"
+#include "LCD.h"
+#include "Trigger.h"
+#include "Icu_software.h"
+#include "Distance.h"
+#include "application.h"
 
 int main()
-{
+{	float32 TON , distance   ;
 	LCD_INIT_4bit();
-	//trigger function from ehab
+	external_interrupt_init();
+	Trigger_int ();
+	Trigger_Pulse ();
+	while(1!=running_flag);
+	running_flag=0;
+
 	while(1)
 	{
+		TON = Time_Calc();
+		distance = Calculate_Distance (TON);
+		distance_alarm(distance);
 		sint8 detect_object_result = detect_object();
 		uint32 counter_dectected_object = count_object(detect_object_result);
 		if (detect_object_result == 1)
@@ -16,6 +31,6 @@ int main()
 		{
 			led_off(LED0);
 		}
-		distance_alarm(distance);
+
 	}
 }
