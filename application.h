bae2d0b37d8bdca257176262c17ffe58f7e19188
MinaRaoufAziz <55676799+MinaRaoufAziz@users.noreply.github.com>
2019-10-01 18:35:53 +0200

    Update application.h

--- application.h
+++ application.h
@@ -5,7 +5,11 @@
 #define LED1 1
 #define LED2 2
 #define LED3 3
-#define MAX_DELAY_FACTOR 20
+#define MAX_DELAY_FACTOR 30
+#define MIN_DELAY_FACTOR 10
+#define AVG_DELAY_FACTOR 20
+#define MAX_DISTANCE 40
+#define MIN_DISTANCE 10
 
 sint8 detect_object(void);
 uint32 count_object(sint8);
