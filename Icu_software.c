e8a8f654d768d378b15b8cdb4be5549814c4a7e6
Mostafa-Ashry <55672180+Mostafa-Ashry@users.noreply.github.com>
2019-10-01 19:14:10 +0200

    Icu Sotware Without testing

---
+++ Icu_software.c
@@ -0,0 +1,50 @@
+/*
+ * Icu_software.c
+ *
+ * Created: 10/1/2019 1:13:28 PM
+ *  Author: Ashry
+ */ 
+#include "macros.h"
+#include "stdtypes.h"
+#include "registers.h"
+#include <avr/interrupt.h>
+uint8 volatile Flag_edge=0;
+uint16 volatile Counts=0;
+
+void Rising_edge(){
+	REG_SREG|=(1<<I_BIT);
+    REG_GICR|=(1<<INT2_BIT);	
+    REG_MCUCSR|=(1<<6);
+	Flag_edge=0;
+	
+	
+}
+void Falling_edge(){	
+REG_SREG|=(1<<I_BIT);
+REG_GICR|=(1<<INT2_BIT);
+REG_MCUCSR&=~(1<<6);	
+}
+ISR(INT2_vect){
+	if(Flag_edge==0){
+	  TCCR1B|=0b00000010;
+	  TCNT1=0;
+	  Flag_edge++;
+	  Falling_edge();	
+		
+	}
+	else if(Flag_edge==1){
+		Counts=TCNT1;
+		Flag_edge=0;
+		Rising_edge();
+	}
+	
+	
+	
+	
+	
+}
+float Time_Calc(){
+	float x=0;
+	x=Counts/1000;
+	return x;
+}
---
+++ Icu_software.h
@@ -0,0 +1,18 @@
+/*
+ * Icu_software.h
+ *
+ * Created: 10/1/2019 5:22:32 PM
+ *  Author: Ashry
+ */ 
+
+
+#ifndef ICU_SOFTWARE_H_
+#define ICU_SOFTWARE_H_
+
+
+
+void Rising_edge();
+void Falling_edge();
+float Time_Calc();
+
+#endif /* ICU_SOFTWARE_H_ */
