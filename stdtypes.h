87f423dd3270ea163d728d2cbf4f78514ef2e786
MinaRaoufAziz <55676799+MinaRaoufAziz@users.noreply.github.com>
2019-10-01 21:02:18 +0200

    Create stdtypes.h

---
+++ stdtypes.h
@@ -0,0 +1,14 @@
+/* Utility File Created By: Mina Raouf
+In this file we define new standard datatypes
+*/
+#ifndef stdTypes_h
+#define stdTypes_h
+typedef unsigned char uint8;
+typedef signed char sint8;
+typedef unsigned short int uint16;
+typedef signed short int sint16;
+typedef unsigned long int uint32;
+typedef signed long int sint32;
+typedef float float32;
+typedef double f64;
+#endif
