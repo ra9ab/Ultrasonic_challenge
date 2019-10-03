0f459e1aa57491b06a976456f26b11d2494ed7b5
Mohamed Ragab <50461567+ra9ab@users.noreply.github.com>
2019-10-04 00:04:34 +0200

    updating lcd driver

--- LCD.h
+++ LCD.h
@@ -1,32 +1,69 @@
+ /******************************************************************************
+ *
+ * Module: LCD
+ *
+ * File Name: lcd.h
+ *
+ * Description: Header file for the LCD driver
+ *
+ * Author: Mohamed Tarek
+ *
+ *******************************************************************************/
 
 #ifndef LCD_H_
 #define LCD_H_
 
+#include "stdtypes.h"
+#include "macros.h"
 
-#define F_CPU 8000000ul
-#include <avr/io.h>
-#include <util/delay.h>
-#define DATA_PORT PORTA
-#define CMD_PORT PORTB
-
-#define DATA_DDR DDRA
-#define CMD_DDR DDRB
-#define RS 1
-#define EN 3
-#define RW 2
-
-void Enable();
-void CMD_WRT_8bit(uint8_t cmd);
-void CMD_WRT_4bit(uint8_t cmd);
-void DATA_WRT_8bit(uint8_t data);
-void DATA_WRT_4bit(uint8_t data);
-void LCD_INIT_4bit();
-void LCD_CLR();
-void LCD_Cursor_Home();
-void LCD_CUSRSOR_POS(uint8_t row , uint8_t col);
-void LCD_CHAR_DISP(uint8_t row , uint8_t col , uint8_t data);
-void LCD_String_DISP(uint8_t row , uint8_t col ,char *s);
-void LCD_NUM_DISP(uint8_t row , uint8_t col ,int num);
-
-
+#ifndef F_CPU
+#define F_CPU 1000000UL //1MHz Clock frequency
 #endif
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+/*******************************************************************************
+ *                      Preprocessor Macros                                    *
+ *******************************************************************************/
+/* LCD Data bits mode configuration */
+#define DATA_BITS_MODE 4
+
+/* Use higher 4 bits in the data port */
+#if (DATA_BITS_MODE == 4)
+#define UPPER_PORT_PINS
+#endif
+
+/* LCD HW Pins */
+#define RS PA1
+#define RW PA2
+#define E  PA3
+#define LCD_CTRL_PORT PORTA
+#define LCD_CTRL_PORT_DIR DDRA
+
+#define LCD_DATA_PORT PORTA
+#define LCD_DATA_PORT_DIR DDRA
+
+/* LCD Commands */
+#define CLEAR_COMMAND 0x01
+#define FOUR_BITS_DATA_MODE 0x02
+#define TWO_LINE_LCD_Four_BIT_MODE 0x28
+#define TWO_LINE_LCD_Eight_BIT_MODE 0x38
+#define CURSOR_OFF 0x0C
+#define CURSOR_ON 0x0E
+#define SET_CURSOR_LOCATION 0x80
+
+/*******************************************************************************
+ *                      Functions Prototypes                                   *
+ *******************************************************************************/
+void LCD_sendCommand(uint8 command);
+void LCD_displayCharacter(uint8 data);
+void LCD_displayString(const char *Str);
+void LCD_init(void);
+void LCD_clearScreen(void);
+void LCD_displayStringRowColumn(uint8 row,uint8 col,const char *Str);
+void LCD_goToRowColumn(uint8 row,uint8 col);
+void LCD_intgerToString(int data);
+
+#endif /* LCD_H_ */
